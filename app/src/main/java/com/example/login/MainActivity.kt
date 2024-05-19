4216ee3ea5b8ab611191123ecd96aef8fd5ddfa2
wramos98 <wilfredo.ramos@tecsup.edu.pe>
2024-05-19 08:27:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.login
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.login.R.id.btnLogin
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val emailValido: String = "wilfredo@gmail.com"
+        val passwordValido: String = "12345"
+
+        val etEmail : EditText = findViewById(R.id.etEmail)
+        val etPassword : EditText = findViewById(R.id.etPassword)
+        val btnLogin : Button = findViewById(R.id.btnLogin)
+
+        btnLogin.setOnClickListener{
+            val email: String = etEmail.text.trim().toString()
+            val password : String = etPassword.text.trim().toString()
+
+            if (email == emailValido && password == passwordValido){
+                val intent = Intent(this,homeActivity::class.java)
+                startActivity(intent)
+            }
+
+            val rootView : View = findViewById(android.R.id.content)
+            Snackbar.make(rootView, "Credenciales invalidas", Snackbar.LENGTH_LONG).show()
+
+
+        }
+
+
+
+    }
+}
